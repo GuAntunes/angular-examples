3711ce3ded4273835fa271bb6d5b0a4c5cc36ea0
Gustavo Antunes <gas.antunes84@gmail.com>
2020-04-24 22:02:03 -0300

    Property and Event Binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,6 +10,9 @@ export class DataBindingComponent implements OnInit {
   url: string = 'http://gustavoantunes.com.br';
   cursoAngular: boolean = true;
   urlImagem: string = 'http://lorempixel.com/400/200/sports';
+  valorAtual : string = '';
+  valorSalvo : string = '';
+  isMouseOver : boolean = false;
 
   constructor() { }
 
@@ -20,8 +23,25 @@ export class DataBindingComponent implements OnInit {
     return 1;
   }
 
-  getCurtirCurso(){
+  getCurtirCurso() {
     return true;
   }
 
+  botaoClicado() {
+    alert("Botão Clicado!")
+  }
+
+  onKeyUp(event : KeyboardEvent) {
+
+    this.valorAtual = (<HTMLInputElement>event.target).value;
+
+  }
+
+  salvarValor(valor : string){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
 }
