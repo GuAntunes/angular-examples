745589f5904d79aaff2e497fff1def893060e6ea
Gustavo Antunes <gas.antunes84@gmail.com>
2020-04-24 22:34:03 -0300

    Utilizando input property

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { FormsModule } from '@angular/forms';
+import { InputPropertyComponent } from './input-property/input-property.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent,
+    InputPropertyComponent,
   ],
   imports: [
     BrowserModule,
