6472ad8283236c814fa93e9d5e877d449af3a47f
Gustavo Antunes <gas.antunes84@gmail.com>
2020-05-15 20:09:40 -0300

    Criado modulo alunos, utilizando rotas filhas

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { CursosModule } from './cursos/cursos.module';
+import { AlunosModule } from './alunos/alunos.module';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { CursosModule } from './cursos/cursos.module';
   imports: [
     BrowserModule,
     CursosModule,
+    AlunosModule,
     AppRoutingModule
     // routing
   ],
