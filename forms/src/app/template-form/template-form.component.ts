b43186c8118f7769d21e957ceda63c5b68e26ab6
Gustavo Antunes <gas.antunes84@gmail.com>
2020-05-20 17:14:29 -0300

    Criado formularios com campos de endereço, realizando a chamada para completar os dados

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template-form',
@@ -12,14 +14,73 @@ export class TemplateFormComponent implements OnInit {
     email: null
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(form){
+  onSubmit(form) {
     console.log(form);
     console.log(this.usuario);
+  }
+
+  consultaCEP(cep, form) {
+    cep = cep.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+
+      //Valida o formato do CEP.
+      if (validacep.test(cep)) {
+
+        this.resetaDadosForm(form);
+        
+        this.http.get(`https://viacep.com.br/ws/${cep}/json/`).subscribe((dados) => this.populaDadosForm(dados, form));
+      }
+    }
+  }
+
+  populaDadosForm(dados, form) {
+    // form.setValue({
+    //   "nome": form.value.nome,
+    //   "email": form.value.email,
+    //   "endereco": {
+    //     "cep": dados.cep,
+    //     "numero": "",
+    //     "complemento": dados.complemento,
+    //     "rua": dados.logradouro,
+    //     "bairro": dados.bairro,
+    //     "cidade": dados.localidade,
+    //     "estado": dados.uf
+    //   }
+    // });
+
+    form.form.patchValue({
+      "endereco": {
+        "cep": dados.cep,
+        "complemento": dados.complemento,
+        "rua": dados.logradouro,
+        "bairro": dados.bairro,
+        "cidade": dados.localidade,
+        "estado": dados.uf
+      }
+    });
+  }
+
+  resetaDadosForm(form) {
+    form.form.patchValue({
+      "endereco": {
+        "cep": null,
+        "complemento": null,
+        "rua": null,
+        "bairro": null,
+        "cidade": null,
+        "estado": null
+      }
+    });
   }
 
 }
--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -1,19 +1,24 @@
 import { FormsModule } from '@angular/forms';
-import { TemplateFormComponent } from './template-form.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 import { FormDebugComponent } from '../form-debug/form-debug.component';
+import { CampoControlComponent } from './../campo-control/campo-control.component';
+import { TemplateFormComponent } from './template-form.component';
 
 
 
 @NgModule({
   declarations: [
     TemplateFormComponent,
-    FormDebugComponent
+    FormDebugComponent,
+    CampoControlComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
+    HttpClientModule
   ]
 })
 export class TemplateFormModule { }
