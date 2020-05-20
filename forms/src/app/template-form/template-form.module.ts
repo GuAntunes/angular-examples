b800863f423e852bbf940b819436b5154090afb6
Gustavo Antunes <gas.antunes84@gmail.com>
2020-05-20 10:43:50 -0300

    Criado projeto de forms - template driven

---
+++ forms/src/app/template-form/template-form.module.ts
@@ -0,0 +1,19 @@
+import { FormsModule } from '@angular/forms';
+import { TemplateFormComponent } from './template-form.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormDebugComponent } from '../form-debug/form-debug.component';
+
+
+
+@NgModule({
+  declarations: [
+    TemplateFormComponent,
+    FormDebugComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+  ]
+})
+export class TemplateFormModule { }
